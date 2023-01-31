539111ecf8ce691f217fe0f430dc42c449602024
Eren <erencan_inanci@hotmail.com>
2023-02-01 01:30:30 +0300

    baseView and resultView update

---
+++ src/js/views/baseView.js
@@ -0,0 +1,66 @@
+import icons from 'url:../../img/icons.svg'; //Parcel 2
+
+class BaseView {
+  _data;
+
+  render(data) {
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `
+        <div class="error">
+          <div>
+             <svg>
+                <use href="s${icons}#icon-alert-triangle"></use>
+             </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+        <div class="message">
+          <div>
+             <svg>
+                <use href="${icons}#icon-smile"></use>
+             </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(event =>
+      window.addEventListener(event, handler)
+    );
+  }
+}
+
+export default BaseView;
